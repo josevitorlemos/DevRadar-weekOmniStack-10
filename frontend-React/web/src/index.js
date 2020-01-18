ab5020ad643ceed92535a4a97565bc76be4ffbc2
José Vitor <jose81073175@gmail.com>
2020-01-18 03:56:21 -0300

    Developing fronend with React

---
+++ frontend-React/web/src/index.js
@@ -0,0 +1,6 @@
+import React from 'react'; //importante pois possibilita o JSX
+import ReactDOM from 'react-dom'; //import árvore de elementos DOM
+import App from './App'; //Código HTML
+
+ReactDOM.render(<App />, document.getElementById('root')); //Enviando JSX para injetar no HTML
+
