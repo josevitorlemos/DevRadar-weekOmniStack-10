73d72372ef5525e4891abb986768549e01b520f1
José Vitor <jose81073175@gmail.com>
2020-01-18 23:59:26 -0300

    Updates - Developing frontend-React

--- frontend-React/web/src/App.js
+++ frontend-React/web/src/App.js
@@ -1,5 +1,9 @@
 import React, { useState } from 'react';
 import Header from './Header'
+import './stylesGlobal.css';
+import './App.css';
+import './Sidebar.css';
+import './Main.css';
 
 //Componente: É um bloco de código com HTML, CSS e JS, a qual pode ser criado para reúso e não interfere na aplicação.
 
@@ -13,18 +17,93 @@ import Header from './Header'
 
 
 function App() {
-  const [counter, setCounter] = useState(0); //Desestruturando o que o useState retorna, pegar um objeto/array e dividir em variáveis.
-
-  function IncrementCounter() {
-    setCounter(counter + 10);
-  }
 
   return (
-    <>
-      <h1>Contator: {counter}</h1>
-      <button onClick={IncrementCounter}>Incrementar</button>
-    </>
+    <div id='app'>
+      <aside>
+        <strong>Cadastrar</strong>
+        <form>
+          <div class="input-block">
+            <label htmlFor="github_username">Usuário do GitHub</label>
+            <input name="github_username" id="username_github" required />
+          </div>
+
+          <div class="input-block">
+            <label htmlFor="techs">Tecnologias</label>
+            <input name="techs" id="techs" required />
+          </div>
+
+          <div className="input-group">
+            <div class="input-block">
+              <label htmlFor="Latitude">Latitude</label>
+              <input name="Latitude" id="Latitude" required />
+            </div>
+
+          </div>
+
+          <div className="input-block">
+            <label htmlFor="Longitude">Longitude</label>
+            <input name="Longitude" id="Longitude" required />
+          </div>
+
+          <button type="submit">Salvar</button>
+        </form>
+      </aside>
+
+      <main>
+        <ul>
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/50563095?s=400&u=3eb053e1d6a66c5e32117b4d485aee6dc55a9146&v=4" alt="José Vitor" />
+              <div className="user-info">
+                <strong>José Vitor</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Student of Information System at Católica Tocantins/2018,</p>
+            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/50563095?s=400&u=3eb053e1d6a66c5e32117b4d485aee6dc55a9146&v=4" alt="José Vitor" />
+              <div className="user-info">
+                <strong>José Vitor</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Student of Information System at Católica Tocantins/2018,</p>
+            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/50563095?s=400&u=3eb053e1d6a66c5e32117b4d485aee6dc55a9146&v=4" alt="José Vitor" />
+              <div className="user-info">
+                <strong>José Vitor</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Student of Information System at Católica Tocantins/2018,</p>
+            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
+          </li>
+
+          <li className="dev-item">
+            <header>
+              <img src="https://avatars2.githubusercontent.com/u/50563095?s=400&u=3eb053e1d6a66c5e32117b4d485aee6dc55a9146&v=4" alt="José Vitor" />
+              <div className="user-info">
+                <strong>José Vitor</strong>
+                <span>ReactJS, React Native, Node.js</span>
+              </div>
+            </header>
+            <p>Student of Information System at Católica Tocantins/2018,</p>
+            <a href="https://github.com/diego3g">Acessar perfil no Github</a>
+          </li>
+        </ul>
+      </main>
+    </div>
   );
 }
 
 export default App;
+
